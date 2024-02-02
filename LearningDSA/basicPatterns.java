ce872ceddbe6635436512e9a918caaf0d3a47baf
BHUPESH PATEL <bhupesh.patel.92372@gmail.com>
2024-02-02 21:02:04 +0530

    learning DSA
    :x
    :x
    
    :x
    :x

---
+++ LearningDSA/basicPatterns.java
@@ -0,0 +1,85 @@
+package LearningDSA;
+
+public class basicPatterns {
+    
+    public static void main(String[] args) {
+       System.out.println("PATTERN 1");
+        //PATTERN 1 ALL STAR
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=5;j++)
+            {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        } 
+        
+        System.out.println("PATTERN 2");
+
+        //PATTERN 2
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=5;j++)
+            {
+                if(j==1||j==5||i==1||i==5) 
+                System.out.print("*");
+                else
+                System.out.print(" ");
+                
+            }
+            System.out.println();
+        } 
+
+        //PATTERN 3
+        System.out.println("PATTERN 3");
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        } 
+        
+        //PATTERN 4
+        System.out.println("PATTERN 4");
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=5;j>=i;j--)
+            {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        } 
+        //PATTERN 5
+        System.out.println("PATTERN 5");
+        for(int i=5;i>=1;i--)
+        {
+            for(int j=1;j<=5;j++)
+            {
+                if(j>=i)
+                System.out.print("*");
+                else
+                System.out.print(" ");
+
+            }
+            System.out.println();
+        } 
+         //PATTERN 6
+         System.out.println("PATTERN 6");
+         for(int i=1;i<=5;i++)
+         {
+             for(int j=1;j<=i;j++)
+             {
+              
+                 System.out.print(j);
+                  
+ 
+             }
+             System.out.println();
+         } 
+    }
+}
