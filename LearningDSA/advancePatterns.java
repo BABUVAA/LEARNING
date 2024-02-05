e61688164fb06edc3a5f3f3f0f16208c860f67a8
BHUPESH PATEL <bhupesh.patel.92372@gmail.com>
2024-02-05 12:13:23 +0530

    HANDS ON DSA

--- LearningDSA/advancePatterns.java
+++ LearningDSA/advancePatterns.java
@@ -10,30 +10,30 @@ public class advancePatterns {
  
  for(int i = 1; i <=4;i++)
  {
-     for(int j=1;j<=i;j++)
+     for(int j=1;j<=i;j++) //PRINT STARS
      {
       System.out.print("*");
      }
-     for(int j=1;j<=(4-i)*2;j++)
+     for(int j=1;j<=(4-i)*2;j++) // PRINT SPACES
      {System.out.print(" ");}
     
-     for(int j=1;j<=i;j++)
+     for(int j=1;j<=i;j++) // PRINT STARS
      {
      System.out.print("*");
      }          
      System.out.println();
  }        
  //second half butterfly
- for(int i = 4; i >=1;i--)
+ for(int i = 4; i >=1;i--) 
  {
-     for(int j=1;j<=i;j++)
+     for(int j=1;j<=i;j++) //PRINT STARS
      {
       System.out.print("*");
      }
-     for(int j=1;j<=(4-i)*2;j++)
+     for(int j=1;j<=(4-i)*2;j++) //PRINT SPACES
      {System.out.print(" ");}
     
-     for(int j=1;j<=i;j++)
+     for(int j=1;j<=i;j++) //PRINT STARS
      {
      System.out.print("*");
      }          
@@ -46,9 +46,9 @@ public class advancePatterns {
         System.out.println("PATTERN RHOMBUS");
         for(int i=1;i<=n;i++)
         {
-            for(int j=1;j<=n-i;j++)
+            for(int j=1;j<=n-i;j++) //PRINT SPACES
             {System.out.print(" ");}
-            for(int j=1;j<=5;j++)
+            for(int j=1;j<=5;j++)  //PRINT STARS
             {
                 System.out.print("*");
             }
@@ -60,14 +60,14 @@ public class advancePatterns {
         System.out.println("PATTERN EMPTY RHOMBUS");
         for(int i=1;i<=n;i++)
         {
-            for(int j=1;j<=n-i;j++)
+            for(int j=1;j<=n-i;j++) //PRINT SPACES
             {System.out.print(" ");}
-            for(int j=1;j<=n;j++)
+            for(int j=1;j<=n;j++)   //RHOMBUS ITERATON
             {
-                if(j==1||j==n||i==1||i==n)
+                if(j==1||j==n||i==1||i==n) //RHOMBUS OUTSIDE STARS
                 System.out.print("*");
                 else
-                System.out.print(" ");
+                System.out.print(" "); //RHOMBUS INSIDE SPACES
             }
             System.out.println();
         }
@@ -76,11 +76,11 @@ public class advancePatterns {
         System.out.println("PATTERN NUMBERPYRAMID");
         for(int i=1;i<=n;i++)
         {
-            for(int j=1;j<=n-i;j++)
+            for(int j=1;j<=n-i;j++) // PRINT SPACES
             {
-                System.out.print(" ");
+                System.out.print(" "); 
             }
-            for(int j=1;j<=i;j++)
+            for(int j=1;j<=i;j++) //PRINT NUMBER PYRAMID
             {
                 System.out.print(i+" ");
             }
@@ -92,15 +92,15 @@ public class advancePatterns {
     System.out.println("PATTERN PALLINDROME NUMBER PYRAMID");
         for(int i=1;i<=n;i++)
         {
-            for(int j=1;j<=n-i;j++)
+            for(int j=1;j<=n-i;j++)  //PRINT SPACES
             {
                 System.out.print(" ");
             }
-            for(int j=i;j>=1;j--)
+            for(int j=i;j>=1;j--)  //PRINT HALF NUMBER PYRAMID
             {
                 System.out.print(j);
             }
-            for(int j=2;j<=i;j++)
+            for(int j=2;j<=i;j++) // PRINT OTHER HALF PYRAMID
             {
                 System.out.print(j);
             }
@@ -112,34 +112,34 @@ public class advancePatterns {
         System.out.println(" PATTERN DIAMOND");
         int n=4;
        
-        for(int i=1;i<=n;i++)
+        for(int i=1;i<=n;i++) // PRINTING FIRST UPPER HALF  
         {
-            for(int j=1;j<=n-i;j++)
+            for(int j=1;j<=n-i;j++)  // PRINT SPACES
             {
                 System.out.print(" ");
             }
-            for(int j=1;j<=i;j++)
+            for(int j=1;j<=i;j++)  //PRINT LEFT HALF STAR PYRAMID
             {
                 System.out.print("*");
             }
             
-            for(int j=2;j<=i;j++)
+            for(int j=2;j<=i;j++)  //PRINT RIGHT HALF STAR PYRAMID
             {
                 System.out.print("*");
             }
             System.out.println();
         }
-        for(int i=1;i<=n-1;i++)
+        for(int i=1;i<=n-1;i++) //PRINTING LOWER HALF PYRAMID
         {
-            for(int j=n;j>n-i;j--)
+            for(int j=n;j>n-i;j--) //PRINTING SPACES
             {
                 System.out.print(" ");
             }
-            for(int j=i;j<=n-1;j++)
+            for(int j=i;j<=n-1;j++) //PRINT LEFT HALF STAR PYRAMID
             {
                 System.out.print("*");
             }
-            for(int j=i;j<n-1;j++)
+            for(int j=i;j<n-1;j++) // PRINT RIGHT HALF STAR PYRAMID
             {
                 System.out.print("*");
             }
