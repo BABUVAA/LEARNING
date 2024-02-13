825f04d22a6cd25804c7594f863b44f55fd3bc67
BHUPESH PATEL <BHOOPS.PATEL.92372@GMAIL.COM>
2024-02-13 14:16:13 +0530

    correcting files

--- Learning-JAVA/DayEnums.java
+++ Learning-JAVA/DayEnums.java
@@ -3,7 +3,6 @@ public enum DayEnums {
     SUNDAY("ITS SUNDAY"),MONDAY("ITS MONDAY"),TUESDAY("ITS TUESDAY"),WEDNESDAY("ITS WEDNESDAY"),THRUSDAY("ITS THRUSDAY"),FRIDAY("ITS FRIDAY"),SATURDAY("ITS SATURDAY");
 
     private String day;
-    private int num;
     DayEnums(String day)
     {
         this.day=day;
