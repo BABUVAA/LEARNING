2151c690a1882b9f04f7aef53af81bfac0eebd02
BHUPESH PATEL <BHOOPS.PATEL.92372@GMAIL.COM>
2024-04-29 10:32:23 +0530

    optimized

--- Learning-JAVA/DP/knapsack/subsetSumProblem.java
+++ Learning-JAVA/DP/knapsack/subsetSumProblem.java
@@ -1,18 +1,26 @@
 package DP.knapsack;
 
+import java.util.HashMap;
+
+
 public class subsetSumProblem {
     
-private static boolean subsetSumProblemBrute(int[] nums, int target, int n)
+private static boolean subsetSumProblemBrute(int[] nums, int target, int n,HashMap<Integer,Boolean> hashMap)
 {  
     if(target==0)
         return true;
     if( n-1 == -1)
         return false;    
-   
+    if(hashMap.containsKey(target))
+        return hashMap.get(target);
+
 if(nums[n-1]<=target)
-    return subsetSumProblemBrute(nums, target-nums[n-1], n-1) || subsetSumProblemBrute(nums, target, n-1) ;
+   {
+    hashMap.put(target, (subsetSumProblemBrute(nums, target-nums[n-1], n-1, hashMap) || subsetSumProblemBrute(nums, target, n-1, hashMap)))     ;
+    return hashMap.get(target);}
 else
-    return subsetSumProblemBrute(nums, target, n-1);
+   { hashMap.put(target, subsetSumProblemBrute(nums, target, n-1, hashMap));
+    return  hashMap.get(target);}
 }
 
 
@@ -20,10 +28,11 @@ else
 
 //Driver Code
     public static void main(String[] args) {
+    HashMap <Integer, Boolean>hashMap=new HashMap<>();
       int []nums={2,3,7,8,10};
-      int target=0;  
+      int target=2;  
         int n=nums.length;
-        System.out.println(subsetSumProblemBrute(nums,target,n));
+        System.out.println(subsetSumProblemBrute(nums,target,n,hashMap));
 
     }
 }
