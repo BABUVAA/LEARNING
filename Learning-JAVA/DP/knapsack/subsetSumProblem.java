ac12b8d48745e9b241f9a3404ee34cc239ff6e8c
BHUPESH PATEL <BHOOPS.PATEL.92372@GMAIL.COM>
2024-04-29 10:09:15 +0530

    problem subset sum solved

--- Learning-JAVA/DP/knapsack/subsetSumProblem.java
+++ Learning-JAVA/DP/knapsack/subsetSumProblem.java
@@ -5,15 +5,14 @@ public class subsetSumProblem {
 private static boolean subsetSumProblemBrute(int[] nums, int target, int n)
 {  
     if(target==0)
-    return true;
-
-
-
-    if(nums[n-1]<target)
-        return subsetSumProblemBrute(nums, target-nums[n-1], n-1);
-    else
-    return subsetSumProblemBrute(nums, target, n-1);    
-
+        return true;
+    if( n-1 == -1)
+        return false;    
+   
+if(nums[n-1]<=target)
+    return subsetSumProblemBrute(nums, target-nums[n-1], n-1) || subsetSumProblemBrute(nums, target, n-1) ;
+else
+    return subsetSumProblemBrute(nums, target, n-1);
 }
 
 
@@ -22,7 +21,7 @@ private static boolean subsetSumProblemBrute(int[] nums, int target, int n)
 //Driver Code
     public static void main(String[] args) {
       int []nums={2,3,7,8,10};
-      int target=17;  
+      int target=0;  
         int n=nums.length;
         System.out.println(subsetSumProblemBrute(nums,target,n));
 
