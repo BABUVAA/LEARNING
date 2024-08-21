b170091f4a19030fdab0319250cf5b1cdc375e38
BHUPESH PATEL <BHOOPS.PATEL.92372@GMAIL.COM>
2024-08-21 11:28:53 +0530

    correcting error

--- Learning-REACT/Bharat-clock-version-1/src/App.jsx
+++ Learning-REACT/Bharat-clock-version-1/src/App.jsx
@@ -1,7 +1,6 @@
 import AppName from "./component/AppName"
 import ClockSlogan from "./component/ClockSlogan"
 import ClockTime from "./component/ClockTime"
-import './App.css'
 function App() {
  
   return <center >
