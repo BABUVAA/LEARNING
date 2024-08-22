a3632e465b2552b73c4d24697a1677f219058370
BHUPESH PATEL <BHOOPS.PATEL.92372@GMAIL.COM>
2024-08-22 20:14:45 +0530

    new methods

--- Learning-REACT/Learning-props/src/components/ErrorMessage.jsx
+++ Learning-REACT/Learning-props/src/components/ErrorMessage.jsx
@@ -2,7 +2,7 @@
 const ErrorMessage = (props) => {
   let zooAnimal = [];
   //let zooAnimal =["Lion", "Cheeta", "Giraffe", "Elephant", "Fox", "Zebra"];
-  let emptyMessage = props.fetch.length === 0 ? <h3>Zoo is empty</h3>: null;
+  let emptyMessage = props.fetch.length === 0 && <h3>Zoo is empty</h3>;
   return <>
       <h3>{emptyMessage}</h3>
   </>
