a16645cd3692c97768084668000e1122e0c6e22a
BHUPESH PATEL <BHOOPS.PATEL.92372@GMAIL.COM>
2024-08-22 18:00:03 +0530

    learn map conditional rendering

--- Learning-REACT/Fragments/src/App.jsx
+++ Learning-REACT/Fragments/src/App.jsx
@@ -1,8 +1,11 @@
 
 function App() {
+ // let foodItems =[];
   let foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", "Milk"];
+  let emptyMessage = foodItems.length === 0 ?<h3>I am still Hungry</h3> :null
   return  <>
   <h1>Healthy Food (Using map)</h1>
+    { emptyMessage}
   <ul className="list-group">
       {foodItems.map(item => <li key={item} className="list-group-item">{item}</li>)}
   </ul>
