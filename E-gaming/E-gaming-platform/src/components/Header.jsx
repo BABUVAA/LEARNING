ca8ce7e4c9eb9a358e1ca23d0f5a42e0c0311ec9
BHUPESH PATEL <BHOOPS.PATEL.92372@GMAIL.COM>
2024-08-28 10:56:03 +0530

    changes

--- E-gaming/E-gaming-platform/src/components/Header.jsx
+++ E-gaming/E-gaming-platform/src/components/Header.jsx
@@ -6,27 +6,27 @@ import HeaderNavBar from "./headersubComponents/headerNavBar";
 import Login from "./headersubComponents/Login.jsx";
 import SignUp from "./headersubComponents/SignUp.jsx";
 const Header = () => {
- 
-
   return (
     <>
-    <header>
-    <div className={styles.headerContainer}>
-       <Logo/>
-       <div className={styles.headerNavDrop}>
-        <PlatformDropdown/>
-        <HeaderNavBar/> 
+      <header>
+        <div className={styles.headerContainer}>
+          <div className={styles.elogoContainer}>
+            <Logo />
+            <div className={styles.headerNavDrop}>
+              <PlatformDropdown />
+              <HeaderNavBar />
+            </div>
+          </div>
+
+          <div className={styles.LogConatiner}>
+            <LanguageDropDown />
+            <Login />
+            <SignUp />
+          </div>
         </div>
-        <div className={styles.LogConatiner}>
-        <LanguageDropDown />
-        <Login/>
-        <SignUp/>
-        </div>
-      </div>
-      
-    </header>
-    <main>main</main>
-    <footer>footer</footer>
+      </header>
+      <main>main</main>
+      <footer>footer</footer>
     </>
   );
 };
