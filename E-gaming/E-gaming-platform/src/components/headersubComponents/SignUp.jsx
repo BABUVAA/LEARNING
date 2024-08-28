e56440a3f80fb3656d70fe4c8e6023dc09c36fcc
BHUPESH PATEL <BHOOPS.PATEL.92372@GMAIL.COM>
2024-08-28 19:59:53 +0530

    redesigning

--- E-gaming/E-gaming-platform/src/components/headersubComponents/SignUp.jsx
+++
@@ -1,6 +0,0 @@
-import styles from "./SignUp.module.css"
-const SignUp = () =>
-  {
-    return  <div class={styles.loginbuttoncontainer}><button class={styles.signupbutton} role="login">Log in</button></div>
-  }
-  export default SignUp
