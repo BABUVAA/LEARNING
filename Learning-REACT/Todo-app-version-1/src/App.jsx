cd29879ec5053ae62c3a58488739038760d31aee
BHUPESH PATEL <BHOOPS.PATEL.92372@GMAIL.COM>
2024-09-13 10:31:45 +0530

    learning

--- Learning-REACT/Todo-app-version-1/src/App.jsx
+++ Learning-REACT/Todo-app-version-1/src/App.jsx
@@ -1,19 +1,44 @@
 import AppName from "./component/AppName";
 import AddTodo from "./component/AddTodo";
 import DeleteTodo from "./component/DeleteTodo";
-import DeleteTodo2 from "./component/DeleteTodo2";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
- return <div className='todo-container'>
-  <AppName/>
-  <AddTodo/>
-  <div className="items-container">
-  <DeleteTodo/>
-  <DeleteTodo2/>
-  </div>
- </div>
- 
+  const initialItems = [
+    {
+      name: "Buy Milk",
+      dueDate: "04/10/2024",
+    },
+    {
+      name: "Go to College",
+      dueDate: "08/10/2024",
+    },
+    {
+      name: "Buy Car",
+      dueDate: "12/12/2024",
+    },
+  ];
+
+  const [todoItems, setTodoItems] = useState(initialItems);
+
+  const onNewItem =(itemName,itemDueDate)=>{
+    setTodoItems([...todoItems,{name:itemName,dueDate:itemDueDate}])
+  }
+
+  const onDeleteClick=(todoItems)=>{
+  };
+  return (
+    <div className='todo-container'>
+      <AppName />
+      <AddTodo handleNewItem={onNewItem}/>
+      <div className='items-container'>
+        {todoItems.map((todoItem) => (
+          <DeleteTodo name={todoItem.name} dueDate={todoItem.dueDate} onHandleDelete={onDeleteClick()} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- Learning-REACT/Todo-app-version-1/src/component/AddTodo.jsx
+++ Learning-REACT/Todo-app-version-1/src/component/AddTodo.jsx
@@ -1,16 +1,47 @@
-function AddTodo(){
-return <div className="container">
-<div class="row b-row">
-<div class="col-6">
-  <input type="text" placeholder="Enter TODO Here"></input>
-</div>
-<div class="col-4">
-  <input type="date"></input>
-</div>
-<div class="col-2">
-  <button type="button" class="btn btn-success b-button">Add</button>
-</div>
-</div>
-</div>
+import { useState } from "react";
+
+function AddTodo({ handleNewItem }) {
+  const [todoName, setTodoName] = useState();
+  const [todoDate, setTodoDate] = useState();
+
+  const handleNameChange = (event) => {
+    setTodoName(event.target.value);
+  };
+  const handleDateChange = (event) => {
+    setTodoDate(event.target.value);
+  };
+  const handleButtonClick = () => {
+    handleNewItem(todoName, todoDate);
+    setTodoName("");
+    setTodoDate("");
+  };
+
+  return (
+    <div className='container'>
+      <div class='row b-row'>
+        <div class='col-6'>
+          <input
+            type='text'
+            value={todoName}
+            placeholder='Enter TODO Here'
+            onChange={handleNameChange}></input>
+        </div>
+        <div class='col-4'>
+          <input
+            type='date'
+            value={todoDate}
+            onChange={handleDateChange}></input>
+        </div>
+        <div class='col-2'>
+          <button
+            type='button'
+            class='btn btn-success b-button'
+            onClick={handleButtonClick}>
+            Add
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
-export default AddTodo
+export default AddTodo;
